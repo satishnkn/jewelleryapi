154a28b858b33b409893004e7ec28d5f77ffb512
unknown <viswanath>
2018-03-03 15:09:00 +0530

    tag model added

---
+++ src/main/java/com/jewellerypos/api/model/Tag.java
@@ -0,0 +1,100 @@
+package com.jewellerypos.api.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+public class Tag implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8288702134276276640L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "TAG_NO")
+	private long tagNo;
+	
+	@Column(name = "TAG_ID")
+	private String tagId;
+			
+	
+	@Column(name = "TAG_PLUS_DATE")
+	private LocalDateTime tagPlusDate;
+		
+	
+	@Column(name = "PRODUCT_CODE")
+	private long productCode;
+	
+	@Column(name = "PIECE")
+	private int piece;
+	
+	@Column(name = "GROSS_WEIGHT")
+	private double grossWeight;
+	
+	@Column(name = "NET_WEIGHT")
+	private double netWeight;
+	
+	@Column(name = "LESS_WEIGHT")
+	private double lessWeight;
+	
+	@Column(name = "PURCHASE_RATE")
+	private double purchaseRate;
+	
+	@Column(name = "WASTAGE")
+	private double wastage;
+	
+	@Column(name = "MAKING_CHARGE")
+	private double makingCharge;
+	
+	@Column(name = "GROSS_OR_NET")
+	private String grossOrNet;
+	
+	@Column(name = "DISCOUNT")
+	private double discount;
+		
+	@Column(name = "OTHER_CHARGE")
+	private double otherCharge;
+	
+	@OneToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            mappedBy = "purchase")
+	private Set<PurchaseAddon> purchaseAddon;
+	
+	@Column(name = "SALE_BILL_NO")
+	private String saleBillNo;
+	
+	@Column(name = "TAG_MINUS_DATE")
+	private LocalDateTime tagMinusDate;
+		
+	@Column(name = "OPERATOR_CODE")
+    private long operatorCode;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)  
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Column(name = "CREATED_ON")
+    private LocalDateTime createdOn;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)  
+    @JsonSerialize(using = LocalDateTimeSerializer.class)  
+    @Column(name = "UPDATED_ON")
+    private LocalDateTime updatedOn;
+
+}
---
+++ src/main/java/com/jewellerypos/api/restcontroller/TagController.java
@@ -0,0 +1,17 @@
+package com.jewellerypos.api.restcontroller;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Path("/")
+@Consumes({ "application/json" })
+@Produces({ "application/json" })
+public class TagController {
+	
+	
+
+}
